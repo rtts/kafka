06b7edc9e4cdce74cff70fa2bf3e13d2e02abe34
Jaap Joris Vens <jj@rtts.eu>
2018-10-24 16:41:57 +0200

    Automated commit for deployment

--- project/urls.py
+++ project/urls.py
@@ -11,4 +11,5 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('', include(kafka.urls)),
     #path('game/', include(game.urls)),
     path('ddw/', include(game.urls)),
+    path('DDW/', include(game.urls)),
 ]
