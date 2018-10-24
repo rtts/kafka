4e47a4b1e257c4f0b1bd6e3347ee1608a32f65b7
Jaap Joris Vens <jj@rtts.eu>
2018-10-24 16:30:22 +0200

    Automated commit for deployment

--- game/urls.py
+++ game/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', GameView.as_view(), name='game'),
+    path('game/', GameView.as_view(), name='game'),
     path('<int:screen_id>/', GameScreenView.as_view(), name='screen'),
-    path('start/', TitleScreenView.as_view(), name='title_screen'),
+    path('', TitleScreenView.as_view(), name='title_screen'),
     path('choose_character/', ChooseCharacterView.as_view(), name='choose_character'),
     path('character/', CharacterView.as_view(), name='character'),
     path('graph/', graph, name='graph'),
